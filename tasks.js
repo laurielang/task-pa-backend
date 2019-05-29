ac73c95aaa5794176b3b9a19d05fef58f8dd03e4
laurielang <laurielang@hotmail.co.uk>
2019-05-29 15:33:42 +0100

    completed array function

--- tasks.js
+++ tasks.js
@@ -8,10 +8,16 @@ app.get('/tasks', function (request, response) {
 
   const username = request.query.username;
 
+  let tasks = [{taskDescrition: "nsoos", complete: false}, {taskDescrition: "nsoos", complete: false}, {taskDescrition: "nsoos", complete: false}, {taskDescrition: "nsoos", complete: false}];
+
   const someJson = {
-    message: "Hello " + username + ", how are you?"
+    message: "Hello " + username + ", how are you?" + " Here are your tasks" ,
+    
+    tasks: tasks
   };
   response.json(someJson);
 })
+
+
 
 module.exports.handler = serverless(app);
