a6980514d45bdf20347c128dddc2f60385ee4f57
laurielang <laurielang@hotmail.co.uk>
2019-05-30 20:55:20 +0100

    connected API to RDS

--- tasks.js
+++ tasks.js
@@ -2,21 +2,37 @@ const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql');
 app.use(cors());
 
-app.get('/tasks', function (request, response) {
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "Task_List"
+});
 
+app.get("/tasks", function(request, response) {
   const username = request.query.username;
-
-  let tasks = [{taskDescrition: "nsoos", complete: false}, {taskDescrition: "nsoos", complete: false}, {taskDescrition: "nsoos", complete: false}, {taskDescrition: "nsoos", complete: false}];
-
-  const someJson = {
-    message: "Hello " + username + ", how are you?" + " Here are your tasks" ,
-    
-    tasks: tasks
-  };
-  response.json(someJson);
-})
+  let query = "SELECT * FROM Task_Data";
+  // if (username) {
+  //   query =
+  //     "SELECT * FROM Task JOIN User on Task_Data.UserId = User.UserId WHERE User.Username = " +
+  //     connection.escape(username);
+  // }
+  connection.query(query, (err, queryResults) => {
+    if (err) {
+      console.log("Error fetching tasks", err);
+      response.status(500).json({
+        error: err
+      });
+    } else {
+      response.json({
+        tasks: queryResults
+      });
+    }
+  });
+});
 
 
 
